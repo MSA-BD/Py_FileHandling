9a8ef9564f9828e2ed333af614fdf0ef01f94b36
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-12 22:29:53 +0600

    Explain files

--- fileHandlingIntro.py
+++ fileHandlingIntro.py
@@ -1,1 +1,6 @@
 # What is file handling in python?
+'''
+Files: Files are named location on disk to store information.
+Files are used to store data parmanently. Files are saved on
+non-volatile memmory. We can retrieve data whenever required.
+'''
