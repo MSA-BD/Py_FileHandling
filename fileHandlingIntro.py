9face1187c104173aac128fb512bafc0f26bb708
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-12 23:05:41 +0600

    Built in open method

--- fileHandlingIntro.py
+++ fileHandlingIntro.py
@@ -3,4 +3,15 @@
 Files: Files are named location on disk to store information.
 Files are used to store data parmanently. Files are saved on
 non-volatile memmory. We can retrieve data whenever required.
+There aare two types of files :
+1. Text Files( text, json. yaml, csv,etc file)
+2. Binary Files( Audion, Video, PDF,Image etc files)
+What is file handling:
+ 1. Open the file,
+ 2.Admit some 0peration on file,
+ 3. Close the file
 '''
+fileObj=open('names.txt',mode='r')
+if fileObj:
+    print('File opend successfully')
+
