8b8713f29a2f05197673ed1435687aa2bd369e8e
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-14 16:14:47 +0600

    Merging multiple files

--- mergedFiles.py
+++ mergedFiles.py
@@ -2,18 +2,36 @@ from os import path
 existFiles=[]
 mergedFilesData=''
 def addFilesToMerged():
- while True:
-    try:
-        addFile=input('Enter filename to merged: ')+'.txt'
-        if path.isfile(addFile)=='False':
-            return 'File does not exist'
-        else:
-            existFiles.append(addFile)
-    except FileExistsError as error:
-        return error
-    finally:
-        addMore=input('Dou want to add more?(y/n)')
-        if addMore !='y':
-            break
+    while True:
+        addFileName = input('Enter file name to merged: ') + '.txt'
+        try:
+            isExistFile = path.isfile(addFileName)
+            print(isExistFile)
+            if isExistFile=='True':
+                # print(isExistFile)
+                existFiles.append(addFileName)
+                addMore = input('Do you want to add more file?(y/n): ')
+                if addMore != 'y':
+                        break
+            else:
+                raise FileExistsError('File does not exist')
+                addMore = input('Do you want to add more file?(y/n):')
+                if addMore != 'y':
+                    break
+                else:
+                    continue
+        except FileExistsError as error:
+            print(f"{addFileName} file does not exist")
+            addMore = input('Do you want to add more file?(y/n): ')
+            if addMore != 'y':
+                break
+            else:
+                continue
+        # finally:
+        #     addMore = input('Do you want to add more file?(y/n): ')
+        #     if addMore != 'y':
+        #         break
+
+
 addFilesToMerged()
 print(existFiles)
